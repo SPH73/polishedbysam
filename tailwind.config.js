a3b28d000951e1692b971f62fad14110b83b78cb
Sue Holder <susanpholder@gmail.com>
2023-01-08 10:23:20 +0000

    added display fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-  'components/**/*.{vue,js,ts}',
-  'layouts/**/*.vue',
-  'pages/**/*.vue',
-  'composables/**/*.{js,ts}',
-  'plugins/**/*.{js,ts}',
-  'App.{js,ts,vue}',
-  'app.{js,ts,vue}',
-  'Error.{js,ts,vue}',
-  'error.{js,ts,vue}',
-  'content/**/*.md',
-],
+    "components/**/*.{vue,js,ts}",
+    "layouts/**/*.vue",
+    "pages/**/*.vue",
+    "composables/**/*.{js,ts}",
+    "plugins/**/*.{js,ts}",
+    "App.{js,ts,vue}",
+    "app.{js,ts,vue}",
+    "Error.{js,ts,vue}",
+    "error.{js,ts,vue}",
+    "content/**/*.md",
+  ],
   theme: {
-    fontFamily: {},
-     screens: {},
+    fontFamily: {
+      poiret: ["PoiretOne-Regular", "sans-serif"],
+      questrial: ["Questrial-Regular", "sans-serif"],
+    },
+    screens: {},
     container: {
       center: true,
-       padding: {},
+      padding: {},
     },
     extend: {
       colors: {},
     },
   },
   plugins: [],
-}
+};
