821d549e125886209a91f803f72013c282a65f6f
Sue Holder <susanpholder@gmail.com>
2023-01-18 17:42:28 +0000

    page added waiting for content

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <section>
+    <div class="text-center mx-auto">
+      <div class="container">
+        <h1>About</h1>
+      </div>
+    </div>
+  </section>
+</template>
