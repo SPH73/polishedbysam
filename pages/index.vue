facb99fff4008fd53c22bf382254e0ea1dbdf843
Sue Holder <susanpholder@gmail.com>
2023-01-18 14:45:14 +0000

    added text content

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,26 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+definePageMeta({
+  layout: "construction",
+});
+</script>
 
 <template>
   <section>
-    <div class="text-center mx-auto">
-      <div class="container relative font-sans">
-        <h1 class="uppercase mb-8 text-3xl">Polished By Sam</h1>
-        <h2 class="mb-8 text-xl">Website under construction!
-        </h2>
-        <p class="mb-8 text-xl">Please check back soon!</p>
-        <p class="mb-8 text-xl">You can also visit our social media pages</p>
-      </div>
+    <div class="mx-20">
+      <h1 class="text-primary text-6xl">
+        Website is <br />
+        <span class="uppercase">UNDER CONSTRUCTION</span>
+      </h1>
+      <p class="tracking-wide">
+        My mobile nail service is still available; please contact me via
+        FaceBook: @polishedbysam or WhatsApp: 07379 385222
+      </p>
+      <p>
+        If you would like to hear about upcoming offers, services and new
+        products, please consider subscribing to my mailing list. No spam ever,
+        just info!
+      </p>
+      <button>Subscribe</button>
     </div>
   </section>
 </template>
-
-<style scoped>
-
-</style>
